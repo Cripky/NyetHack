9333dd3fa65780b2264e38158f1e7177a70f6ceb
Maxim Pescherov <maxpesherov@gmail.com>
2022-06-18 13:36:45 +0500

    Home 16.06.22

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -31,8 +31,7 @@ private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean
     return auraColor
 }
 
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
-    val healthStatus = when (healthPoints) {
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean) = when (healthPoints) {
         100 -> "is in excellent condition!"
         in 90..99 -> "has a few scratches."
         in 75..89 -> if (isBlessed) {
@@ -40,10 +39,7 @@ private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
         } else "has some minor wounds."
         in 15..74 -> "looks pretty hurt."
         else -> "is in awful condition!"
-    }
-    return healthStatus
 }
 
-private fun castFireball(numFireballs: Int = 2) {
+private fun castFireball(numFireballs: Int = 2) =
     println("A glass of Fireball springs into existence. (x$numFireballs)")
-}
