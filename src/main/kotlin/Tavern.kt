1fcbad53c58d4e3553a4e9d7e1e251bf048326dd
pesherov <maxpesherov@gmail.com>
2022-06-20 15:03:56 +0500

    Job 20.06.22

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,4 +1,8 @@
 fun main() {
-    var beverage = readLine()?.capitalize()
-    println(beverage)
+    var beverage = readLine()
+        if (beverage != null) {
+            beverage = beverage.capitalize()
+        } else println("I can't do that without crashing - beverage wan null!")
+    val beverageServed: String = beverage?: "Buttered Ale"
+    println(beverageServed)
 }
