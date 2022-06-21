6776cc1ac824f8af26ae08902339a1bfd7ffb0a6
pesherov <maxpesherov@gmail.com>
2022-06-21 15:56:46 +0500

    Job 21.06.22

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,8 +1,29 @@
+const val TAVERN_NAME = "Taernyl's Folly"
+
 fun main() {
-    var beverage = readLine()
-        if (beverage != null) {
-            beverage = beverage.capitalize()
-        } else println("I can't do that without crashing - beverage wan null!")
-    val beverageServed: String = beverage?: "Buttered Ale"
-    println(beverageServed)
+    placeOrder("shandy,Dragon's Breath,5.91")
+}
+
+private fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("Madrigal speaks with $tavernMaster about their order.")
+
+    val (type, name, price) = menuData.split(',')
+    val message = "Madrigal buys a $name ($type) for $price"
+    println(message)
+
+    val phrase = "Ah, delicious $name!"
+    println("Madrigal exclaims: ${toDragonSpeak(phrase)}")
+}
+
+private fun toDragonSpeak(phrase: String) = phrase.replace(Regex("[aeiou]")) {
+    when (it.value) {
+        "a" -> "4"
+        "e" -> "3"
+        "i" -> "1"
+        "o" -> "0"
+        "u" -> "|_|"
+        else -> it.value
+    }
 }
