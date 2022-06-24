d1ba6c23d171015426185ae4ed6b2a7aa7c8377e
pesherov <maxpesherov@gmail.com>
2022-06-24 09:05:06 +0500

    Job 24.06.22

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -17,9 +17,9 @@ fun main() {
         println("The tavern master says: Yea, they're seated by the stew kettle.")
     } else println("The tavern master says: Nay, they departed hours ago.")
 
-    placeOrder("shandy,Dragon's Breath,5.91")
-    for (patron in patronList) {
-        println("Good evening, $patron")
+    patronList.forEachIndexed {
+        index, patron -> println("Good evening, $patron - you're #${index + 1} in line.")
+        placeOrder(patron, "shandy,Dragon's Breath,5.91")
     }
 }
 
@@ -43,20 +43,20 @@ private fun displayBalance() {
     println("Player's purse balance: Gold: $playerGold, Silver: $playerSilver")
 }
 
-private fun placeOrder(menuData: String) {
+private fun placeOrder(patronName: String, menuData: String) {
     val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
-    println("Madrigal speaks with $tavernMaster about their order.")
+    println("$patronName speaks with $tavernMaster about their order.")
 
     val (type, name, price) = menuData.split(',')
-    val message = "Madrigal buys a $name ($type) for $price"
+    val message = "$patronName buys a $name ($type) for $price"
     println(message)
 
-    performPurchase(price.toDouble())
+    // performPurchase(price.toDouble())
 
     val phrase = if (name == "Dragon's Breath") {
-        "Madrigal exclaims ${toDragonSpeak("Ah, delicious $name!")}"
-    } else "Madrigal says: Thanks for the $name."
+        "$patronName exclaims ${toDragonSpeak("Ah, delicious $name!")}"
+    } else "$patronName says: Thanks for the $name."
     println(phrase)
 }
 
