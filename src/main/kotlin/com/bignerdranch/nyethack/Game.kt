2ae7db4cdc2074be999865519db7f281a689646f
pesherov <maxpesherov@gmail.com>
2022-07-04 16:44:58 +0500

    Job 04.07.22

--- src/main/kotlin/com/bignerdranch/nyethack/Game.kt
+++ src/main/kotlin/com/bignerdranch/nyethack/Game.kt
@@ -3,19 +3,33 @@ import com.bignerdranch.nyethack.Room
 import com.bignerdranch.nyethack.TownSquare
 
 fun main() {
-
-    val player = Player("Madrigal")
-    player.castFireball()
-
-    var currentRoom: Room = TownSquare()
-    println(currentRoom.description())
-    println(currentRoom.load())
-
-    // Состояние игрока
-    printPlayerStatus(player)
+    Game.play()
 }
 
-private fun printPlayerStatus(player: Player) {
-    println("(Aura: ${player.auraColor()}) " + "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
-    println("${player.name} ${player.formatHealthStatus()}")
+object Game {
+    private val player = Player("Madrigal")
+    private var currentRoom: Room = TownSquare()
+
+    init {
+        println("Welcome, adventurer!")
+        player.castFireball()
+    }
+
+    fun play() {
+        while (true) {
+            println(currentRoom.description())
+            println(currentRoom.load())
+
+            // Состояние игрока
+            printPlayerStatus(player)
+
+            print("> Enter your command: ")
+            println("Last command: ${readLine()}")
+        }
+    }
+
+    private fun printPlayerStatus(player: Player) {
+        println("(Aura: ${player.auraColor()}) " + "(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
+        println("${player.name} ${player.formatHealthStatus()}")
+    }
 }
